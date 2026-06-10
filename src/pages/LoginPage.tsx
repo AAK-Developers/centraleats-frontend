55d7d8bae03f32cd757cf115818ae65cfdf7cc0b
Kevin-Moyon <kevinjoelvallejo@gmail.com>
2026-06-09 23:36:21 -0500

    refactor: :recycle: Implement atomic design and component composition
    -Atoms: Creation of a Logo and ActionButton to standardize base elements with flexible properties.  -Molecules: Implementation of RoleCard to group presentation and action logic, eliminating code duplication.  -Organisms: Creation of AuthHeader as a centralized container for visual identity and page context.  -Optimization: Refactoring of RoleSelectionPage.tsx using component composition, significantly reducing file complexity and improving readability.

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,27 +1,18 @@
-import { Image, VStack } from '@chakra-ui/react'
 import { SignIn } from '@clerk/clerk-react'
 import { WaveLayout } from '../components/layout/WaveLayout'
-import CentralEatsLogo from '../assets/CentralEats.png'
+import { AuthHeader } from '../components/organisms/AuthHeader'
 
 export default function LoginPage() {
     return (
         <WaveLayout>
-            <VStack gap="0" w="full" mt="-80px">
-                <Image
-                    src={CentralEatsLogo}
-                    alt="CentralEats Logo"
-                    boxSize="250px"
-                    objectFit="contain"
-                />
-                <SignIn
-                    forceRedirectUrl="/role-selection"
+            <AuthHeader logoSize="250px" >
+                <SignIn forceRedirectUrl="/role-selection"
                     appearance={{
                         variables: {
                             colorPrimary: '#E65100'
                         }
-                    }}
-                />
-            </VStack>
+                    }} />
+            </AuthHeader>
         </WaveLayout>
     )
 }
